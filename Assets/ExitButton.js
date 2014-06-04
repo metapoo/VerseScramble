6c2c92a31cac027d4639e9e04992810e586f8c33
Samuel Hsiung <hsiung@gmail.com>
2014-06-03 23:54:47 -0700

    some assets

---
+++ Assets/ExitButton.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var gameManager : GameManager;
+
+function OnMouseDown() {
+	gameManager.Cleanup();
+	Application.LoadLevel("verselist");
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
