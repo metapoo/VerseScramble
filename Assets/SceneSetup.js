18a66209dc55b1b3b24eb1a330b6ce74de48dd3c
Samuel Hsiung <hsiung@gmail.com>
2014-07-16 09:19:41 -0700

    add scene setup

---
+++ Assets/SceneSetup.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+function resizeBackground(mainCam : Camera, background : SpriteRenderer) {
+	if (background.sprite == null) return;
+	var w = background.sprite.bounds.size.x;
+	var h = background.sprite.bounds.size.y;
+	var camW = mainCam.pixelWidth;
+	var camH = mainCam.pixelHeight;
+	var camX = 2*mainCam.ScreenToWorldPoint(new Vector3(camW, 0f, 0f)).x;
+	var camY = 2*mainCam.ScreenToWorldPoint(new Vector3(0f, camH, 0f)).y;
+	background.transform.localScale.x = camX/w;
+	background.transform.localScale.y = camY/h;
+}
+
+function Start () {
+	var background : GameObject = GameObject.Find("BG");
+	var mainCam : GameObject = GameObject.Find("Main Camera");
+	if ((background != null) && (mainCam != null)) {
+		resizeBackground(mainCam.camera, background.GetComponent("SpriteRenderer"));
+	}
+}
+
+function Update () {
+
+}
