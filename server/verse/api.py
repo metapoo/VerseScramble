0d9435009239489a8fac0635464e0af86b4e9742
Samuel Hsiung <hsiung@gmail.com>
2014-08-21 04:29:44 +0000

    api for verse sets

--- server/verse/api.py
+++ server/verse/api.py
@@ -7,14 +7,30 @@ from bson.objectid import ObjectId
 
 def get_handlers():
     return ((r"/api/verse/show", ShowVerseApiHandler),
+            (r"/api/verseset/show", ShowVerseSetApiHandler),
             )
 
 class ShowVerseApiHandler(BaseHandler, ApiMixin):
+    api_name = "verse/show"
     def get(self):
         verse_id = self.get_argument("verse_id")
         verse_id = ObjectId(verse_id)
         verse = Verse.collection.find_one({'_id':verse_id})
         if verse is None:
             self.return_error("verse not found")
-        self.return_success(verse.json())
+        self.return_success({"verse":verse.json()})
 
+class ShowVerseSetApiHandler(BaseHandler, ApiMixin):
+    api_name = "verseset/show"
+    def get(self):
+        verseset_id = self.get_argument("verseset_id")
+        verseset_id = ObjectId(verseset_id)
+        verseset = VerseSet.collection.find_one({'_id':verseset_id})
+        if verseset is None:
+            self.return_error("verse set is not found")
+        
+        verses_json = [verse.json() for verse in verseset.verses()]
+
+        result = {"verseset":verseset.json(),
+                  "verses":verses_json}
+        return self.return_success(result)
