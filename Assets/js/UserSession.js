5c91c2ec1c026b83c9501bdcdba64c5c993a250f
Samuel Hsiung <hsiung@gmail.com>
2014-08-20 21:55:59 -0700

    add NGUI and user session

---
+++ Assets/js/UserSession.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var verse_id : String;
+var verseset_id : String;
+
+function Awake() {
+	DontDestroyOnLoad(this.gameObject);
+}
+
+function Start () {
+}
+
+function Update () {
+
+}
