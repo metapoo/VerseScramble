1e5e57704424a0a7ae5c7b11ce8f8818ad3f9593
Samuel Hsiung <hsiung@gmail.com>
2014-08-26 20:03:14 -0700

    handle url arguments from web

--- Assets/js/UserSession.js
+++ Assets/js/UserSession.js
@@ -18,6 +18,19 @@ function Awake() {
 	options = new Hashtable();
 }
 
+// example URL: verserain://com.hopeofglory.verserain/verse/53ebe35da2ff372bfb9b91f4
+function HandleURL(url : String) {
+
+	var parts = url.Split("/"[0]);
+	var subject = parts[3];
+	var idstr = parts[4];
+	if (subject == "verse") {
+		SetVerseId(idstr);
+	} else if (subject == "verseset") {
+		SetVerseSetId(idstr);
+	}
+}
+
 function SetOption(key : String, value : String) {
 	options[key] = value;
 }
