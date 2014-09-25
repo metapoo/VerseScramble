097ac62bf5a72c06a18a5d62ba830be2f7803285
Samuel Hsiung <hsiung@gmail.com>
2014-09-25 00:28:08 -0700

    get verse sets to display

---
+++ Assets/js/VerseSetButton.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+public var button : Button;
+public var verseset : VerseSet;
+public var label : Text;
+
+function Start () {
+	button = GetComponent(Button);
+	button.onClick.AddListener(HandleOnClick);
+}
+
+function SetVerseSet(vs : VerseSet) {
+	verseset = vs;
+	label.text = vs.setname;
+}
+
+function HandleOnClick() {
+}
+
+function Update () {
+
+}
--- Assets/js/VerseSetsManager.js
+++ Assets/js/VerseSetsManager.js
@@ -1,6 +1,32 @@
 ﻿#pragma strict
+import UnityEngine.UI;
+
+public var scrollContent : RectTransform;
+public var verseManager : VerseManager;
+public var verseSetButton : VerseSetButton;
 
 function Start () {
+	verseManager.LoadVerses();
+	var versesets = verseManager.versesets;
+	var clone : VerseSetButton;
+	var vsButtonLabel : RectTransform = verseSetButton.label.GetComponent(RectTransform);
+	var vsButtonTransform : RectTransform = verseSetButton.GetComponent(RectTransform);
+	var position = 0;
+	var padding = 10;
+	var rowHeight = vsButtonTransform.rect.height;
+	
+	for (var i=0;i<versesets.length;i++) {
+		var verseset : VerseSet = versesets[i];
+		clone = Instantiate(verseSetButton, Vector3.zero, Quaternion.identity);
+		clone.SetVerseSet(verseset);
+		var rt : RectTransform = clone.GetComponent(RectTransform);
+		rt.SetParent(scrollContent, false);
+		rt.localPosition.y = -i*(rowHeight + padding);	
+		var cloneLabel : RectTransform = clone.label.GetComponent(RectTransform);
+		cloneLabel.offsetMin.y = 0;
+		cloneLabel.offsetMax.y = 0;
+		
+	}
 }
 
 function Update () {
