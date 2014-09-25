28d0bb698745c1b51fd10743aee6891138c4bc8b
Samuel Hsiung <hsiung@gmail.com>
2014-09-25 12:21:28 -0700

    make popular/new selectable

---
+++ Assets/js/NavigationButton.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+public var button : Button;
+public var label : Text;
+private var normalColor : Color;
+static var selectedButton : NavigationButton = null;
+
+
+function Start () {
+	button = GetComponent(Button);
+	normalColor = button.colors.normalColor;
+	button.onClick.AddListener(HandleOnClick);
+}
+
+function HandleOnClick() {
+	Highlight();
+}
+
+function Highlight() {
+	var rt : RectTransform = GetComponent(RectTransform);	
+	if (selectedButton != null) {
+		selectedButton.UnHighlight();
+	}
+	selectedButton = this;
+	button.colors.normalColor = button.colors.highlightedColor;
+}
+
+function UnHighlight() {
+	button.colors.normalColor = normalColor;
+}
+
+function Update () {
+
+}
