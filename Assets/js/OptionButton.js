fd2844653126702ac48f6378551f5c32d5a0165c
Samuel Hsiung <hsiung@gmail.com>
2014-09-30 22:26:08 -0700

    add option button/dialog

---
+++ Assets/js/OptionButton.js
@@ -0,0 +1,22 @@
+var labelText : Text;
+
+function SetLabel(label : String) {
+	labelText.text = label;
+}
+
+function SetParent(prt : RectTransform) {
+	var rt : RectTransform = GetComponent(RectTransform);
+	var oldPosition = rt.anchoredPosition;
+	var oldScale = rt.localScale;
+	
+	rt.SetParent(prt);
+	
+	rt.anchoredPosition = oldPosition;
+	rt.localScale = oldScale;	
+}
+
+function Start() {
+}
+
+function Update() {
+}
---
+++ Assets/js/OptionDialog.js
@@ -0,0 +1,65 @@
+﻿#pragma strict
+
+var optionButton : OptionButton;
+var title : String;
+var description : String;
+var titleText : Text;
+var descriptionText : Text;
+var numOptions : int = 0;
+
+function HandleOkayButtonClick() {
+	Destroy(this.gameObject);
+}
+
+function SetParent(prt : RectTransform) {
+	var rt : RectTransform = GetComponent(RectTransform);
+	var oldPosition = rt.anchoredPosition;
+	var oldScale = rt.localScale;
+	
+	rt.SetParent(prt);
+	
+	rt.anchoredPosition = oldPosition;
+	rt.localScale = oldScale;
+	
+	SetTitle("TITLE");
+	SetDescription("DESCRIPTION");
+	
+}
+
+function SetTitle(_title : String) {
+	titleText.text = TextManager.GetText(_title);
+	title = _title;
+}
+
+function SetDescription(_description : String) {
+	descriptionText.text = TextManager.GetText(_description);
+	description = _description;
+}
+
+function CloseDialog() {
+	Destroy(this.gameObject);
+}
+
+function AddOption(label : String, handler : Function) {
+	numOptions += 1;
+	var optButton = Instantiate(optionButton, Vector2.zero, Quaternion.identity);
+	optButton.SetParent(GetComponent(RectTransform));
+	optButton.SetLabel(label);
+	optButton.GetComponent(Button).onClick.AddListener(handler);
+	optButton.GetComponent(Button).onClick.AddListener(CloseDialog);
+	var rt : RectTransform = optButton.GetComponent(RectTransform);
+	var windowPadding = 60;
+	var buttonPadding = 30;
+	var height = (rt.sizeDelta.y + buttonPadding);
+	rt.anchoredPosition.y = windowPadding + height * (numOptions - 1);
+	GetComponent(RectTransform).sizeDelta.y += height;
+}
+
+function Start () {
+	SetTitle(title);
+	SetDescription(description);
+}
+
+function Update () {
+
+}
