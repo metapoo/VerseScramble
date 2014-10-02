75a9aa796174f33d10c7deda20d9f447cb2238dc
Samuel Hsiung <hsiung@gmail.com>
2014-10-02 07:32:08 +0000

    fix typo

--- server/login/api.py
+++ server/login/api.py
@@ -1,4 +1,4 @@
-B1;2cfrom verserain.base.handler import BaseHandler
+from verserain.base.handler import BaseHandler
 from verserain.login.auth import *
 from verserain.api.api import *
 from bson.objectid import ObjectId
