9195f7bb27e35f3f77721f3afcafe4ec2e621c34
Samuel Hsiung <hsiung@gmail.com>
2014-10-09 06:54:54 +0000

    work on hash validation of scores

--- server/leaderboard/api.py
+++ server/leaderboard/api.py
@@ -30,10 +30,13 @@ class LeaderboardVersesetScoresHandler(BaseHandler, ApiMixin):
 
 class LeaderboardSubmitScoreHandler(BaseHandler, ApiMixin):
     api_name="leaderboard/verseset/submit_score"
+    secret_key="0>a-q,wYTmq%<,h$OXYg<js:h([TR/:4hSVh.vEJhq4RvWIx@_|^B|]z`b<d~kI@"
+
     def get(self):
         return self.post()
 
     def post(self):
+        import md5
         response = {"is_logged_in":False}
 
         if self.current_user is None:
@@ -41,6 +44,15 @@ class LeaderboardSubmitScoreHandler(BaseHandler, ApiMixin):
 
         score = self.get_int_argument("score")
         verseset_id = ObjectId(self.get_argument("verseset_id"))
+
+        hash_target = "%s-%s-%s-%s" % (str(self.current_user._id),
+                                       str(verseset_id),
+                                       str(score),
+                                       str(LeaderboardSubmitScoreHandler.secret_key))
+
+        expected_hash = md5.new(hash_target).hexdigest()
+        submitted_hash = self.get_argument("hash","")
+        validated = (expected_hash == submitted_hash)
 
         VersesetScore.submit_score(user_id=self.current_user._id,
                                    username=self.current_user['username'],
@@ -50,4 +62,6 @@ class LeaderboardSubmitScoreHandler(BaseHandler, ApiMixin):
 
         response = get_scores_json(verseset_id)
         response["is_logged_in"] = True
+        response["validated"] = validated
+
         return self.return_success(response)
