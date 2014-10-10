d51975354b4e64cb50ca0710f0e0cfaef040db96
Samuel Hsiung <hsiung@gmail.com>
2014-10-10 10:51:14 -0700

    some localization

--- Assets/js/VerseButton.js
+++ Assets/js/VerseButton.js
@@ -37,7 +37,7 @@ function SetVerse(v : Verse) {
 		highScore = verseset.GetMetadata()["high_score"];
 		label.text = String.Format("{0} - {1}: {2}",
 			TextManager.GetText("Play Challenge (All Verses)"),
-			TextManager.GetText("high"),
+			TextManager.GetText("High"),
 			highScore); //this is what will be written in the rows
 		var versesetDifficulty : int = verseset.GetMetadata()["difficulty"];
 		button.colors.normalColor = GetColorForDifficulty(versesetDifficulty);
