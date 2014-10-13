41059e66360a69b1fd2ba9178643417b2700730a
Samuel Hsiung <hsiung@gmail.com>
2014-10-13 19:37:08 +0000

    contact page and submission

--- server/base/base.py
+++ server/base/base.py
@@ -1,16 +1,19 @@
 from verserain.base.handler import BaseHandler
 from verserain.login.auth import *
 from verserain.verse.models import *
+from verserain import settings
 
 def get_handlers():
     return ((r"/?", FrontPageHandler),
             (r"/privacy/?", PrivacyPageHandler),
             (r"/search/?", SearchPageHandler),
-            )
+            (r"/contact/submit/?", ContactHandler),
+            (r"/contact/?", ContactPageHandler),
+    )
 
 class PrivacyPageHandler(BaseHandler):
     def get(self):
-        self.render("privacy.html")
+        self.render("privacy.html", selected_nav="about")
 
 class FrontPageHandler(BaseHandler):
     def get(self, path=None):
@@ -19,3 +22,30 @@ class FrontPageHandler(BaseHandler):
 class SearchPageHandler(BaseHandler):
     def get(self):
         self.render("search.html", selected_nav="search")
+
+class ContactPageHandler(BaseHandler):
+    def get(self):
+        message_sent = self.get_boolean_argument("message_sent",False)
+        self.render("contact.html", selected_nav="about", message_sent=message_sent)
+
+class ContactHandler(BaseHandler):
+    def post(self):
+        import smtplib
+        from_email = "admin@%s" % settings.SITE_DOMAIN
+        reply_to_email = self.get_argument("email")
+        to_email = ["sam@%s" % settings.SITE_DOMAIN] # must be a list
+        subject = self.get_argument('subject')
+        message = self.get_argument('message')
+        # Prepare actual message
+        message = """From: %s
+To: %s
+Subject: %s
+Reply-To: %s
+%s
+""" % (from_email, ", ".join(to_email), subject, reply_to_email, message)
+        # Send the mail
+        server = smtplib.SMTP('eternityinourheart.com',port=25)
+        server.sendmail(from_email, to_email, message)
+        server.quit()
+
+        self.redirect("/contact?message_sent=True")
