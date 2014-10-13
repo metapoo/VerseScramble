c1f09b1ed4027ed4c1991856853c64f30ad63dfc
Samuel Hsiung <hsiung@gmail.com>
2014-10-12 22:58:11 -0700

    fix difficulty in challenge mode when theres no window

--- Assets/js/GameManager.js
+++ Assets/js/GameManager.js
@@ -320,6 +320,7 @@ function Start() {
 	   (verseManager.GetCurrentDifficultyAllowed() != Difficulty.Easy)) {
 			ShowDifficultyOptions();
 	} else {
+		verseManager.SetDifficulty(Difficulty.Easy);
 		BeginGame();
 	}
 	
