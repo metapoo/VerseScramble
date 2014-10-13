ac8d26d46418517910193efeb5efbbf144c6d8e2
Zolnamar Dorjsembe <zolnamar@Zolnamars-MacBook-Pro.local>
2014-10-14 00:19:40 +0800

    Added android TTS plugin

--- Assets/plugins/VoiceSynth.cs
+++ Assets/plugins/VoiceSynth.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 public class VoiceSynth{
 /*Interfacetonativeimplementation*/
 
+private	static AndroidJavaObject androidPluginObject;
 [DllImport("__Internal")]
 private static extern void _SpeakUtterance(string text,string language);
 
@@ -12,10 +13,18 @@ private static extern void _SpeakUtterance(string text,string language);
 public static void SpeakUtterance(string text,string language)
 {
 		//Callpluginonlywhenrunningonrealdevice
+		if (Application.platform == RuntimePlatform.Android) {
+			androidPluginObject = new AndroidJavaObject("com.hopeofglory.verserain.VoiceSynthClient");
+			androidPluginObject.Call("Init");
+			androidPluginObject.Call("Speak", text);
+		//	Debug.Log("************************** it works");	
+		}
+
+
 		if((Application.platform!=RuntimePlatform.OSXEditor)&&
-(Application.platform!=RuntimePlatform.OSXWebPlayer)&&
-(Application.platform!=RuntimePlatform.WindowsWebPlayer)){
-		_SpeakUtterance(text,language);
-}
+		   (Application.platform!=RuntimePlatform.OSXWebPlayer)&&
+		   (Application.platform!=RuntimePlatform.WindowsWebPlayer)&&(Application.platform!=RuntimePlatform.Android)){
+			_SpeakUtterance(text,language);
+		}
 }
 }
