237dedb0e689a75c8bba049fce1e2136dc997027
Zolnamar Dorjsembe <zolnamar@Zolnamars-MacBook-Pro.local>
2014-10-15 02:29:45 +0800

    Fixed TTS plugin

--- Assets/plugins/VoiceSynth.cs
+++ Assets/plugins/VoiceSynth.cs
@@ -4,28 +4,23 @@ using System.Runtime.InteropServices;
 
 public class VoiceSynth{
 /*Interfacetonativeimplementation*/
-
-#if UNITY_ANDROID
+	
 private	static AndroidJavaObject androidPluginObject;
-#endif
-
+	
 [DllImport("__Internal")]
 private static extern void _SpeakUtterance(string text,string language);
 
 //Startslookupforsomebonjourregisteredserviceinsidespecifieddomain
 public static void SpeakUtterance(string text,string language)
 {
-#if UNITY_ANDROID
-		//Callpluginonlywhenrunningonrealdevice
 		if (Application.platform == RuntimePlatform.Android) {
 			if (androidPluginObject == null) {
 				androidPluginObject = new AndroidJavaObject("com.hopeofglory.verserain.VoiceSynthClient");
 				androidPluginObject.Call("Init");
 			}
-			androidPluginObject.Call("Speak", text);
-		//	Debug.Log("************************** it works");	
+			androidPluginObject.Call("Speak", new object[] {text, language});
 		}
-#endif
+
 
 		if((Application.platform!=RuntimePlatform.OSXEditor)&&
 		   (Application.platform!=RuntimePlatform.OSXWebPlayer)&&
