9f446b1bde080bad326de9ec555e341a5441b51b
Samuel Hsiung <hsiung@gmail.com>
2014-10-17 05:51:54 +0000

    parse zh locale better

--- server/base/handler.py
+++ server/base/handler.py
@@ -98,8 +98,14 @@ class BaseHandler(tornado.web.RequestHandler, TranslationManager):
         self.set_current_language(language_code)
     
     def default_language(self):
-        locale = self.get_browser_locale()
-        language = locale.code.split("_")[0]
+        locale = self.get_browser_locale().lower()
+
+        if locale == "zh_cn":
+            language = "zh-hans"
+        elif locale == "zh_tw":
+            language = "zh-hant"
+        else:
+            language = locale.code.split("_")[0]
         return language
 
     def render(self, *args, **kwargs):
