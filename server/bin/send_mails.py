70c8bd0f694e2be9a825ac14f2f073960fab4769
Samuel Hsiung <hsiung@gmail.com>
2014-10-20 17:37:24 +0000

    fix problems with email

--- server/bin/send_mails.py
+++ server/bin/send_mails.py
@@ -2,13 +2,18 @@
 from verserain.email.models import *
 import smtplib
 from time import sleep
+from verserain.utils.text import *
 
 def send_mails():
     eqs = list(EmailQueue.collection.find())
     if len(eqs) > 0:
         connection = smtplib.SMTP(settings.IP_ADDRESS,port=25)
         for eq in eqs:
+            if not is_valid_email(eq["to_address"]):
+                eq.remove()
+                continue
             print eq
             eq.send_mail(connection=connection)
+        connection.quit()
 
 send_mails()
