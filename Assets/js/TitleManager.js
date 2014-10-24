b548398e9c8ebab922616877b9573cdda1cf8c53
Samuel Hsiung <hsiung@gmail.com>
2014-10-23 17:55:48 -0700

    auto select language if selected

--- Assets/js/TitleManager.js
+++ Assets/js/TitleManager.js
@@ -5,6 +5,7 @@ import TextManager;
 public var showError : boolean = false;
 public var sndSelect : AudioClip;
 public var background : Transform;
+static public var stayInTitleScreen : boolean;
 
 private	var selectedDifficulty : Difficulty;
 
@@ -18,6 +19,14 @@ function Awake() {
 }
 
 function Start () {
+	if (PlayerPrefs.HasKey("language") && !stayInTitleScreen) {
+		var language : String = VerseManager.GetLanguage();
+		var onFinish : Function = function() {
+			Application.LoadLevel("versesets");
+		};
+		VerseManager.GetInstance().SwitchLanguage(language, onFinish);
+		
+	}
 }
 
 function Update () {
