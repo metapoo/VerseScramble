69fedb64b3d89a1dd4f8e0c358e45da50540d89b
Samuel Hsiung <hsiung@gmail.com>
2014-10-26 22:06:05 +0000

    minify css script for production

--- server/bin/minify_css.py
+++ server/bin/minify_css.py
@@ -1,38 +1,50 @@
 #!/usr/bin/python
 import sys, re
+import os
 
-css = open( sys.argv[1] , 'r' ).read()
+def minify_css(css, out):
+    # remove comments - this will break a lot of hacks :-P
+    css = re.sub( r'\s*/\*\s*\*/', "$$HACK1$$", css ) # preserve IE<6 comment hack
+    css = re.sub( r'/\*[\s\S]*?\*/', "", css )
+    css = css.replace( "$$HACK1$$", '/**/' ) # preserve IE<6 comment hack
+    
+    # url() doesn't need quotes
+    css = re.sub( r'url\((["\'])([^)]*)\1\)', r'url(\2)', css )
 
-# remove comments - this will break a lot of hacks :-P
-css = re.sub( r'\s*/\*\s*\*/', "$$HACK1$$", css ) # preserve IE<6 comment hack
-css = re.sub( r'/\*[\s\S]*?\*/', "", css )
-css = css.replace( "$$HACK1$$", '/**/' ) # preserve IE<6 comment hack
+    # spaces may be safely collapsed as generated content will collapse them anyway
+    css = re.sub( r'\s+', ' ', css )
 
-# url() doesn't need quotes
-css = re.sub( r'url\((["\'])([^)]*)\1\)', r'url(\2)', css )
+    # shorten collapsable colors: #aabbcc to #abc
+    css = re.sub( r'#([0-9a-f])\1([0-9a-f])\2([0-9a-f])\3(\s|;)', r'#\1\2\3\4', css )
 
-# spaces may be safely collapsed as generated content will collapse them anyway
-css = re.sub( r'\s+', ' ', css )
+    # fragment values can loose zeros
+    css = re.sub( r':\s*0(\.\d+([cm]m|e[mx]|in|p[ctx]))\s*;', r':\1;', css )
 
-# shorten collapsable colors: #aabbcc to #abc
-css = re.sub( r'#([0-9a-f])\1([0-9a-f])\2([0-9a-f])\3(\s|;)', r'#\1\2\3\4', css )
+    for rule in re.findall( r'([^{]+){([^}]*)}', css ):
 
-# fragment values can loose zeros
-css = re.sub( r':\s*0(\.\d+([cm]m|e[mx]|in|p[ctx]))\s*;', r':\1;', css )
+        # we don't need spaces around operators
+        selectors = [re.sub( r'(?<=[\[\(>+=])\s+|\s+(?=[=~^$*|>+\]\)])', r'', selector.strip() ) for selector in rule[0].split( ',' )]
 
-for rule in re.findall( r'([^{]+){([^}]*)}', css ):
+        # order is important, but we still want to discard repetitions
+        properties = {}
+        porder = []
+        for prop in re.findall( '(.*?):(.*?)(;|$)', rule[1] ):
+            key = prop[0].strip().lower()
+            if key not in porder: porder.append( key )
+            properties[ key ] = prop[1].strip()
 
-    # we don't need spaces around operators
-    selectors = [re.sub( r'(?<=[\[\(>+=])\s+|\s+(?=[=~^$*|>+\]\)])', r'', selector.strip() ) for selector in rule[0].split( ',' )]
+        # output rule if it contains any declarations
+        if properties:
+            out.write("%s{%s}\n" % ( ','.join( selectors ), ''.join(['%s:%s;' % (key, properties[key]) for key in porder])[:-1] ))
 
-    # order is important, but we still want to discard repetitions
-    properties = {}
-    porder = []
-    for prop in re.findall( '(.*?):(.*?)(;|$)', rule[1] ):
-        key = prop[0].strip().lower()
-        if key not in porder: porder.append( key )
-        properties[ key ] = prop[1].strip()
+css_files = ["application.css","cssmenu.css","pure-min.css"]
+out_file = "verserain.css"
+out_path = "%s/python/verserain/static/stylesheets/%s" % (os.environ['HOME'], out_file)
+out = open(out_path,"w")
 
-    # output rule if it contains any declarations
-    if properties:
-        print "%s{%s}" % ( ','.join( selectors ), ''.join(['%s:%s;' % (key, properties[key]) for key in porder])[:-1] ) 
+for css_file in css_files:
+    css_path = "%s/python/verserain/static/stylesheets/%s" % (os.environ['HOME'], css_file)
+    css = open(css_path,"r").read()
+    minify_css(css, out)
+
+out.close()
