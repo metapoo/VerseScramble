c761378a8ab79d87b3a85ddbb29c0ef9199823e1
Samuel Hsiung <hsiung@gmail.com>
2014-10-29 00:48:59 +0000

    roll fb login code into a method

--- server/fb/fb.py
+++ server/fb/fb.py
@@ -12,6 +12,43 @@ def get_handlers():
     return ((r"/fb/login/?", FacebookGraphLoginHandler),
             (r"/fb/disconnect/?", FacebookDisconnectHandler),
 )
+
+def get_user_from_fb_profile(fb_user, fb_profile):
+    email = None
+
+    if fb_profile.has_key('name'):
+        fb_user['name'] = fb_profile['name']
+
+    if fb_profile.has_key('email'):
+        email = fb_profile['email']
+        fb_user['email'] = email
+    if fb_profile.has_key('gender'):
+        fb_user['gender'] = fb_profile['gender']
+
+    # Save the user with e.g. set_secure_cookie                                                                                                                                                  
+    fb_uid = fb_user["id"]
+    name = fb_user["name"]
+    username = name
+    
+    user = authenticate_login(fb_uid=fb_uid,
+    )
+
+    if user is None and email:
+        user = User.collection.find_one({"email":email})
+        user = None
+
+    if user is None:
+        if User.by_username(username):
+            username = username.replace(" ",".")
+
+        if User.by_username(username):
+            username = username.replace(".","")
+
+        user = create_new_user(fb_uid=fb_uid, name=name, username=username)
+
+    user.handle_fb_user(fb_user)
+    return user
+
 class FacebookGraphLoginHandler(BaseHandler, FacebookGraphMixin):
     @coroutine
     def get(self):
@@ -30,7 +67,6 @@ class FacebookGraphLoginHandler(BaseHandler, FacebookGraphMixin):
 
         fblogin_url = "%s/fb/login" % (self.settings["site_url"],)
         fb_user=None
-        user_created = False
         user = self.current_user
 
         if self.get_argument("code", False):
@@ -41,41 +77,10 @@ class FacebookGraphLoginHandler(BaseHandler, FacebookGraphMixin):
                 code=self.get_argument("code"))
 
             fb_profile = yield self.facebook_request("/me",access_token=fb_user["access_token"])
-            email = None
-
-            if fb_profile.has_key('email'):
-                email = fb_profile['email']
-                fb_user['email'] = email
-            if fb_profile.has_key('gender'):
-                fb_user['gender'] = fb_profile['gender']
-
-            # Save the user with e.g. set_secure_cookie
-            fb_uid = fb_user["id"]
-            name = fb_user["name"]
-            username = name
-            user = self.current_user
-            
-            if User.by_username(username):
-                username = username.replace(" ",".")
-            if User.by_username(username):
-                username = username.replace(".","")
-
-            if user is None:
-                user = authenticate_login(fb_uid=fb_uid, 
-                )
-
-            if user is None and email:
-                user = User.collection.find_one({"email":email})
-
-            if user is None:
-                user = create_new_user(fb_uid=fb_uid, name=name, username=username)
-                user_created = True
-            
-            user.handle_fb_user(fb_user)
+            user = get_user_from_fb_profile(fb_user, fb_profile)
 
             session_key = user.session_key()
             self.set_secure_cookie("session_key", session_key)
-
         else:
             yield self.authorize_redirect(
                 redirect_uri=fblogin_url,
@@ -83,6 +88,7 @@ class FacebookGraphLoginHandler(BaseHandler, FacebookGraphMixin):
                 extra_params={"scope": ["offline_access","email"]})
             return
 
+        fb_uid = user['fb_uid']
         if user.get('password') is None:
             next_url = "/login/reset_password?session_key=%s&fb_uid=%s" % (session_key, fb_uid) 
 
