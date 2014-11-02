3f22e8d983c01365806e77116b15a98b12e337d4
Samuel Hsiung <hsiung@gmail.com>
2014-11-02 16:17:54 +0000

    report correct date for highs

--- server/base/uimodules.py
+++ server/base/uimodules.py
@@ -36,8 +36,9 @@ class VerseSetLink(tornado.web.UIModule):
         return "<a class='link' href='/verseset/show/%s'>%s</a>" % (verseset_id, verseset_name)
 
 class ScoresTable(tornado.web.UIModule):
-    def render(self, scores=None, start_index=0, gt=None):
-        return self.render_string("scores/table.html",scores=scores, start_index=start_index, gt=gt)
+    def render(self, scores=None, start_index=0, gt=None, selected_subnav=None):
+        return self.render_string("scores/table.html",scores=scores, start_index=start_index, gt=gt, 
+                                  selected_subnav=selected_subnav)
 
 class VerseSetsTable(tornado.web.UIModule):
     def render(self, versesets=None, paginator=None, language_code=None, start_index=0, gt=None):
