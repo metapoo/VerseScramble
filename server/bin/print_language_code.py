90928ea6c3be9891392529a5e53e64140cc8dc5f
Samuel Hsiung <hsiung@gmail.com>
2014-11-06 22:43:58 +0000

    imake language scroll view static

--- server/bin/print_language_code.py
+++ server/bin/print_language_code.py
@@ -1,3 +1,5 @@
 from verserain.verse.language import *
+from verserain.utils.encoding import *
+
 for k,v in LANGUAGE_BY_CODE.items():
-    print "\tlanguageByCode.Add('%s','%s');" % (k,v)
+    print "\tlanguageByCode.Add(\"%s\",\"%s\");" % (k,smart_text(v))
