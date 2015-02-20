b977e92818f61c6c3caf5eaf481bf69b4f2d663c
brysonli12 <brysonli@gmail.com>
2015-02-19 19:04:46 -0800

    Added SettingsManager, updated settings.unity
    In this commit, I have added SettingsManager (which is a copy of
    TitleManager (for now) with one small change).  In settings.unity I have
    added some text "Settings Page".

---
+++ Assets/cs/SettingsManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System;
+
+public class SettingsManager:MonoBehaviour{
+	
+	public static bool stayInTitleScreen;
+	
+	public void Awake() {
+		Application.targetFrameRate = 60;
+		string language = VerseManager.GetLanguage();
+		if (!TextManager.IsLoaded()) {
+			TextManager.LoadLanguageOffline(language);
+			TextManager tm = TextManager.GetInstance();
+			StartCoroutine(tm.LoadLanguage(language, null));
+		}
+	}
+	
+	public void LoadVerseSetsMenu(string language) {
+		UserSession us = UserSession.GetUserSession();
+		if (!us.IsUrlDirected()) {			
+		//	Application.LoadLevel("versesets");
+		}
+	}
+	
+	public void Start() {
+		if (PlayerPrefs.HasKey("language") && !stayInTitleScreen) {
+			string language = VerseManager.GetLanguage();
+			VerseManager vm = VerseManager.GetInstance();
+			vm.SwitchLanguage(language, LoadVerseSetsMenu as Action<string>);
+		}
+	}
+	
+	public void Update() {
+	
+	}
+}
+
