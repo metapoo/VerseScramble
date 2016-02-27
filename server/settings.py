267c1420a3031c9fc8df72b3969c2c51d19986b6
samhsiung <hsiung@gmail.com>
2016-02-27 17:58:40 +0000

    fb open graph tags

--- server/settings.py
+++ server/settings.py
@@ -23,6 +23,7 @@ MONGODB_PASSWORD = "teaegg123"
 MONGODB_DATABASE = "verserain"
 SECRET_KEY = "X6yFzlxbSkyB7wjn/dHC49UGP10c7kj0uBj55zORhBw="
 FACEBOOK_API_KEY = "1439577092991935"
+FACEBOOK_APP_ID = "1439577092991935"
 LATEST_VERSION = 1.0
 IOS_APPSTORE_URL = "https://itunes.apple.com/us/app/verse-rain-fun-bible-verse/id928732025?ls=1&mt=8"
 GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.hopeofglory.verserain"
