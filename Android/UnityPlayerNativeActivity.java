5bbccf0d0c36f1c7443cc81d46af01878dcc8274
samhsiung <hsiung@gmail.com>
2016-06-06 10:16:48 -0700

    update main java for android build

--- Android/UnityPlayerNativeActivity.java
+++ Android/UnityPlayerNativeActivity.java
@@ -10,100 +10,93 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.content.Intent;
-import android.net.Uri;
 
 public class UnityPlayerNativeActivity extends NativeActivity
 {
-	protected UnityPlayer mUnityPlayer;		// don't change the name of this variable; referenced from native code
+    protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
 
-	protected void checkIntent() 
-	{
-		Intent intent = getIntent();
-		String scheme = intent.getScheme();
-		
-		if ((scheme != null) && (scheme.equals("verserain"))) {
-			Uri uri = intent.getData();
-			String uriString = uri.toString();	
-			UnityPlayer.UnitySendMessage("UserSession", "HandleURL", uriString);
-		}
+    protected void checkIntent()
+    {
+	Intent intent = getIntent();
+	String scheme = intent.getScheme();
+
+	if ((scheme != null) && (scheme.equals("verserain"))) {
+	    Uri uri = intent.getData();
+	    String uriString = uri.toString();
+	    UnityPlayer.UnitySendMessage("UserSession", "HandleURL", uriString);
 	}
-	
-	@Override protected void onNewIntent(Intent intent)
-	{
-		setIntent(intent);
-		checkIntent();
-		super.onNewIntent(intent);
-	}
-	// Setup activity layout
-	@Override protected void onCreate (Bundle savedInstanceState)
-	{
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		super.onCreate(savedInstanceState);
+    }
 
-		getWindow().takeSurface(null);
-		setTheme(android.R.style.Theme_NoTitleBar_Fullscreen);
-		getWindow().setFormat(PixelFormat.RGB_565);
+    @Override protected void onNewIntent(Intent intent)
+    {
+	setIntent(intent);
+	checkIntent();
+	super.onNewIntent(intent);
+    }
 
-		mUnityPlayer = new UnityPlayer(this);
-		if (mUnityPlayer.getSettings ().getBoolean ("hide_status_bar", true))
-			getWindow ().setFlags (WindowManager.LayoutParams.FLAG_FULLSCREEN,
-			                       WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    // Setup activity layout
+    @Override protected void onCreate (Bundle savedInstanceState)
+    {
+	requestWindowFeature(Window.FEATURE_NO_TITLE);
+	super.onCreate(savedInstanceState);
 
-		setContentView(mUnityPlayer);
-		mUnityPlayer.requestFocus();
-		checkIntent();
+	getWindow().takeSurface(null);
+	getWindow().setFormat(PixelFormat.RGBX_8888); // <--- This makes xperia play happy
 
-	}
+	mUnityPlayer = new UnityPlayer(this);
+	setContentView(mUnityPlayer);
+	mUnityPlayer.requestFocus();
+	checkIntent();
+    }
 
-	// Quit Unity
-	@Override protected void onDestroy ()
-	{
-		mUnityPlayer.quit();
-		super.onDestroy();
-	}
+    // Quit Unity
+    @Override protected void onDestroy ()
+    {
+	mUnityPlayer.quit();
+	super.onDestroy();
+    }
 
-	// Pause Unity
-	@Override protected void onPause()
-	{
-		super.onPause();
-		mUnityPlayer.pause();
-	}
+    // Pause Unity
+    @Override protected void onPause()
+    {
+	super.onPause();
+	mUnityPlayer.pause();
+    }
 
-	// Resume Unity
-	@Override protected void onResume()
-	{
-		super.onResume();
-		mUnityPlayer.resume();
-		checkIntent();
-	}
+    // Resume Unity
+    @Override protected void onResume()
+    {
+	super.onResume();
+	mUnityPlayer.resume();
+	checkIntent();
+    }
 
-	// This ensures the layout will be correct.
-	@Override public void onConfigurationChanged(Configuration newConfig)
-	{
-		super.onConfigurationChanged(newConfig);
-		mUnityPlayer.configurationChanged(newConfig);
-	}
+    // This ensures the layout will be correct.
+    @Override public void onConfigurationChanged(Configuration newConfig)
+    {
+	super.onConfigurationChanged(newConfig);
+	mUnityPlayer.configurationChanged(newConfig);
+    }
 
-	// Notify Unity of the focus change.
-	@Override public void onWindowFocusChanged(boolean hasFocus)
-	{
-		super.onWindowFocusChanged(hasFocus);
-		mUnityPlayer.windowFocusChanged(hasFocus);
-	}
+    // Notify Unity of the focus change.
+    @Override public void onWindowFocusChanged(boolean hasFocus)
+    {
+	super.onWindowFocusChanged(hasFocus);
+	mUnityPlayer.windowFocusChanged(hasFocus);
+    }
 
-	// For some reason the multiple keyevent type is not supported by the ndk.
-	// Force event injection by overriding dispatchKeyEvent().
-	@Override public boolean dispatchKeyEvent(KeyEvent event)
-	{
-		if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
-			return mUnityPlayer.injectEvent(event);
-		return super.dispatchKeyEvent(event);
-	}
+    // For some reason the multiple keyevent type is not supported by the ndk.
+    // Force event injection by overriding dispatchKeyEvent().
+    @Override public boolean dispatchKeyEvent(KeyEvent event)
+    {
+	if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+	    return mUnityPlayer.injectEvent(event);
+	return super.dispatchKeyEvent(event);
+    }
 
-	// Pass any events not handled by (unfocused) views straight to UnityPlayer
-	@Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
-	@Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
-	@Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
-	/*API12*/ public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
+    // Pass any events not handled by (unfocused) views straight to UnityPlayer
+    @Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
+    /*API12*/ public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
 }
