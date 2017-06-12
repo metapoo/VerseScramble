5f0b2f1d47ccff66f1b9fae3b81229b2567ac343
Samuel Hsiung <hsiung@gmail.com>
2017-06-12 02:22:39 +0000

    password for backup file

--- server/bin/backup.py
+++ server/bin/backup.py
@@ -13,8 +13,10 @@ BACKUP_PATH = "%s/backup" % HOME
 dirname = datetime.now().strftime("%a")
 dirpath = "%s/%s" % (BACKUP_PATH, dirname)
 filename = "%s.tar.gz" % dirname
+username = 'admin'
+password = 'teaegg123'
 
-subprocess.Popen(["mongodump --out %s" % dirpath],shell=True).wait()
+subprocess.Popen(["mongodump --out %s -u %s -p %s " % (dirpath, username, password)],shell=True).wait()
 subprocess.Popen(["cd %s;tar cvzf %s %s" % (BACKUP_PATH, filename, dirname)], shell=True).wait()
 subprocess.Popen(["rm -rf %s" % dirpath],shell=True).wait()
 subprocess.Popen(["scp %s/%s hsiung@dev.verserain.com:/home/hsiung/backup" % (BACKUP_PATH, filename)], shell=True).wait()
