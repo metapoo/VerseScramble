01276595c5794155fb3afa797c1d9dcf1ab505f2
Samuel Hsiung <hsiung@gmail.com>
2020-06-13 06:10:50 +0000

    require nonsecure for play

--- server/play/play.py
+++ server/play/play.py
@@ -14,6 +14,7 @@ def get_handlers():
             )
 
 class PlayHandler(BaseHandler):
+    @require_nonsecure
     def get(self):
         language = self.language_code(not_all=True)
         if self.isIOS() and False:
